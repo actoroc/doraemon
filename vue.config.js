678b73f0e194a3a2389706c02d5e786ce4c967c2
actoroc <8641511+actoroc@user.noreply.gitee.com>
2021-02-19 23:45:59 +0800

    '改良'

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
       alias: {
         components: '@/components',
         assets: '@/assets',
+        common:'@/common',
       },
       extensions: ['.js', '.css', '.vue', '.json'],
     },
