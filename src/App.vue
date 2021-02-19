ff5c9bc74cffa353e90dcc8a5af24525459667a0
actoroc <8641511+actoroc@user.noreply.gitee.com>
2021-02-19 23:46:55 +0800

    '改良'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="app">
-    <css-picture></css-picture>
+    <css-picture :cssCode="cssCode" :htmlCode="htmlCode" />
   </div>
 </template>
 
 <script>
 import CssPicture from "components/CssPicture";
+
+import cssCode from "./common/cssCode";
+import htmlCode from "./common/htmlCode";
+
 export default {
   name: "App",
   components: {
     CssPicture,
+  },
+  data() {
+    return {
+      cssCode: cssCode,
+      htmlCode: htmlCode,
+    };
   },
   mounted() {},
 };
